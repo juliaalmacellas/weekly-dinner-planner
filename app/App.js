46aee69bfcaaf5eb29148c125d495113ac41f455
juliaalmacellas <julia.almacellas@gmail.com>
2026-03-05 19:38:17 +0100

    Integrate meal planning functionality into App

--- app/App.js
+++ app/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 import { Text, View } from "react-native";
 
+import { foods } from "../data/foods";
+import { generateMealPlan } from "../logic/generateMealPlan";
+
 export default function App() {
+
+  const plan = generateMealPlan(foods);
+
   return (
     <View>
+
       <Text>Weekly Dinner Planner</Text>
+
+      {plan.map((item, index) => (
+        <Text key={index}>
+          {item.day}: {item.meal}
+        </Text>
+      ))}
+
     </View>
   );
 }
