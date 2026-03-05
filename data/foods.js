62a2c87d00e25dc52b74e2d1b4e0e0efd084d8df
juliaalmacellas <julia.almacellas@gmail.com>
2026-03-05 19:33:40 +0100

    Add initial foods data to foods.js

---
+++ data/foods.js
@@ -0,0 +1,52 @@
+export const foods = [
+  {
+    id: 1,
+    name: "Pizza",
+    category: "FastFood"
+  },
+  {
+    id: 2,
+    name: "Provolone",
+    category: "Cheese"
+  },
+  {
+    id: 3,
+    name: "Soup",
+    category: "Pasta"
+  },
+  {
+    id: 4,
+    name: "Salmon",
+    category: "Fish"
+  },
+  {
+    id: 5,
+    name: "Bacalla",
+    category: "Fish"
+  },
+{
+    id: 6,
+    name: "Pebrot amb Tonyina",
+    category: "Vegetatian"
+  },
+  {
+    id: 7,
+    name: "Truita Espinacs",
+    category: "Eggs"
+  },
+{
+    id: 8,
+    name: "Truita Carbasso",
+    category: "Eggs_"
+  },
+{
+    id: 9,
+    name: "Cheese and Wine",
+    category: "Cheese"
+  },
+{
+    id: 10,
+    name: "Salad and Meat",
+    category: "Healthy"
+  }
+];
